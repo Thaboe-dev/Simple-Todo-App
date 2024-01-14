898d4a0975b3f302d925b0381e6b1d23074fd4e4
Thaboe <thabolezwe.mabandla@students.uz.ac.zw>
2024-01-14 19:45:26 +0200

    updated the model

--- ToDo/models.py
+++ ToDo/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Item(models.Model):
     title = models.CharField(max_length = 50)
     desc = models.CharField(max_length = 500)
-    
+
+    def get_absolute_url(self):
+        return reverse("articles:article-detail", kwargs={"pk": self.id})

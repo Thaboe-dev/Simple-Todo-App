9e3a9807f244c3dc240f930f80b5e85a7fe97faf
Thaboe <thabolezwe.mabandla@students.uz.ac.zw>
2024-01-14 19:00:25 +0200

    created the models and made the migrations

--- ToDo/models.py
+++ ToDo/models.py
@@ -2,3 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
+    title = models.CharField(max_length = 50)
+    desc = models.CharField(max_length = 500)
+    

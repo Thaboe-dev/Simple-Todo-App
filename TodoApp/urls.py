04176012d8772dcfe74c2b1a25f23fb567f490ba
Thaboe <thabolezwe.mabandla@students.uz.ac.zw>
2024-01-29 17:57:29 +0200

    made the app to be the landing url

--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('todo/', include("ToDo.urls")),
+    path('', include("ToDo.urls")),
     path('admin/', admin.site.urls),
     
 ]
